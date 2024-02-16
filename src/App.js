0abd61530da3b27354436b2c8fe02c1375363568
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2024-02-17 00:54:39 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, {useState}from 'react'
-import ImageSearch from './Components/ImageSearch'
+import React, { useState } from 'react';
+import ImageSearch from './Components/ImageSearch';
 import ImageGallery from './Components/ImageGallery';
 
+const App = () => {
+  const [imageList, setImageList] = useState([]);
 
-const App=()=> {
-  const [imageList,setImageList]=useState([]);
   return (
     <div>
-      <div className=' head shadow-lg head1  sticky-top border-none'>
-      <h1 className='d-flex justify-content-md-center pt-3 text-white shadow-lg '>SnapVerse</h1>
-      <p className='d-flex justify-content-md-center text-white shadow '>See the World Through Our Lens </p>
-      <ImageSearch setImageList={setImageList}/>
+      <div className='head shadow-lg head1  sticky-top border-none'>
+        <h1 className='d-flex justify-content-md-center pt-3 text-white shadow-lg '>SnapVerse</h1>
+        <p className='d-flex justify-content-md-center text-white shadow '>See the World Through Our Lens </p>
+        <ImageSearch setImageList={setImageList} />
       </div>
       
-      <ImageGallery imageList={imageList}/>
+      
+      <ImageGallery imageList={imageList} /> 
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/ImageGallery.js
+++ src/Components/ImageGallery.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React from 'react';
 
-const ImageGallery=({imageList})=>{
+const ImageGallery = ({ imageList }) => {
 
-    return(
-        <div className="paro">{
-            imageList.map((item)=>(
-                <img 
-                    src={item.urls.small_s3} 
-                    alt={item.alt_description}
-                   className="image_style pt-5 col-5 mx-2 rounded" />
-            ))
-            }
+  function handleDownload(url) {
+   
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = 'image'; 
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+
+  return (
+    <div className="paro">
+      {imageList.map((item) => (
+        <div key={item.id} className="image-item d-flex  flex-column align-items-center justify-content-around mt-3 " > 
+          <img
+            src={item.urls.small_s3}
+            alt={item.alt_description}
+            className="image_style pt-5 col-16 mx-2 rounded"
+          />
+          <button onClick={() => handleDownload(item.urls.full)} className=" mt-2 text-primary  rounded-circle border">
+           download
+          </button>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
+
 export default ImageGallery;
